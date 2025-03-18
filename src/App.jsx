e11c7afecf8445a796542b873da71ac924f4b38a
Kamoliddin Mirzaboyev <Kamoliddinmirzaboyev05@gmail.com>
2025-03-18 15:36:28 +0500

    Test

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,17 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
 function App() {
-  return <BrowserRouter>
-  <Navbar/>
-  <Routes>
-    <Route />
-  </Routes>
-  <Footer/>
-  </BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="" element={<Home />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
