f4aa645d2b91267b8ecaa986de554f770eb0a27f
Kamoliddin Mirzaboyev <Kamoliddinmirzaboyev05@gmail.com>
2025-03-18 16:49:12 +0500

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,16 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import Home from "./pages/home/Home";
+import SignIn from "./pages/signin/SignIn";
+import SignUp from "./pages/signup/SignUp";
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="" element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
       <Footer />
     </BrowserRouter>
