b3d253a4364fd81679647a16c587982746667ef7
Kamoliddin Mirzaboyev <Kamoliddinmirzaboyev05@gmail.com>
2025-03-19 15:40:53 +0500

    U[date

--- src/pages/signin/SignIn.jsx
+++ src/pages/signin/SignIn.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
 
 function SignIn() {
   return (
     <div>
       <h1>SignIn Start</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi voluptate
+        quos dignissimos, eum itaque tenetur esse praesentium porro tempora,
+        culpa natus totam hic veniam modi ab voluptatibus nihil maxime ullam
+        saepe reiciendis necessitatibus amet! Amet quasi nostrum in vero, sint
+        tenetur nihil! Incidunt ratione obcaecati assumenda. Sequi tempora
+        distinctio error iusto qui nobis illo et voluptate? Minus facilis quia
+        deleniti voluptate qui corporis id similique saepe veniam rerum,
+        quaerat, quisquam perferendis pariatur obcaecati aliquid et beatae nisi
+        maiores, recusandae enim in sunt vitae harum velit! Quos id quidem
+        delectus quis sapiente cum eligendi, non dicta iusto facere animi
+        architecto fugiat?
+      </p>
     </div>
-  )
+  );
 }
 
-export default SignIn
+export default SignIn;
