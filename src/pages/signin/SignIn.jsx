57fcc27b820782e610b27b7251f40769b8950cce
Sarvarbek <sarvarbekodilxojayev898@gmail.com>
2025-03-19 15:42:08 +0500

    starat

--- src/pages/signin/SignIn.jsx
+++ src/pages/signin/SignIn.jsx
@@ -4,19 +4,7 @@ function SignIn() {
   return (
     <div>
       <h1>SignIn Start</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi voluptate
-        quos dignissimos, eum itaque tenetur esse praesentium porro tempora,
-        culpa natus totam hic veniam modi ab voluptatibus nihil maxime ullam
-        saepe reiciendis necessitatibus amet! Amet quasi nostrum in vero, sint
-        tenetur nihil! Incidunt ratione obcaecati assumenda. Sequi tempora
-        distinctio error iusto qui nobis illo et voluptate? Minus facilis quia
-        deleniti voluptate qui corporis id similique saepe veniam rerum,
-        quaerat, quisquam perferendis pariatur obcaecati aliquid et beatae nisi
-        maiores, recusandae enim in sunt vitae harum velit! Quos id quidem
-        delectus quis sapiente cum eligendi, non dicta iusto facere animi
-        architecto fugiat?
-      </p>
+      <p>sarvarbek githup</p>
     </div>
   );
 }
