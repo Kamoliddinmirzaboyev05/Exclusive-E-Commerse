ba8f90e9f52fbddb6f4ab45c0be03262d63b94a8
Sarvarbek <sarvarbekodilxojayev898@gmail.com>
2025-03-21 09:19:47 +0500

    start

--- src/pages/signin/SignIn.jsx
+++ src/pages/signin/SignIn.jsx
@@ -1,11 +1,32 @@
 import React from "react";
+import "./SignIn.css"
 
 function SignIn() {
   return (
-    <div>
-      <h1>SignIn Start</h1>
-      <p>sarvarbek githup</p>
-    </div>
+    <>
+
+      <div className="signIp">
+        <div className="container">
+          <div className='signIpImg'>
+            <img src="/public/Side Image (1).png" alt="" />
+          </div>
+          <div>
+
+          </div>
+          <div className="signIpText">
+            <h1>Log in to Exclusive</h1>
+            <p>Enter your details below</p>
+            <input type="text" placeholder='Email or Phone Number' /><br />
+            <input type="text" placeholder='Password' /><br />
+            <div className="signInIcon">
+              <div><button>Log In</button> </div>
+              <div><p>Forget Password?</p></div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </>
   );
 }
 
--- src/pages/signup/SignUp.jsx
+++ src/pages/signup/SignUp.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import "./SignUp.css"
+import { Link } from 'react-router-dom'
 
 function SignUp() {
   return (
-    <div>SignUp</div>
+    <>
+    <div className="signUp">
+      <div className="container">
+        <div className='signUpImg'>
+          <img src="/public/Side Image (1).png" alt="" />
+        </div>
+        <div>
+
+        </div>
+        <div className="signUpText">
+          <h1>Create an account</h1>
+          <p>Enter your details below</p>
+          <input type="text" placeholder='Name' /><br />
+          <input type="text" placeholder='Email or Phone Number' /><br />
+          <input type="text" placeholder='Password' /><br />
+          <button className='Account'>Create Account</button> <br />
+          <button className='google'><div><img src="/public/Icon-Google.svg" alt="" /></div> <div>Sign up with Google</div></button>
+          <h5>Already have account? <Link to={"/signin"}>Log in</Link></h5>
+        </div>
+      </div>
+    </div>
+    </>
   )
 }
 
